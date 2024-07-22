8cb6493203f14054d3e5a72a39102d5c58f9cb7f
AdameyLaypanov <133885663+AdameyLaypanov@users.noreply.github.com>
2024-07-22 21:24:49 +0300

    update names-delete numerical suffix

--- LeetCode/Easy/Suffix_Sum/006_35. Search Insert Position.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def searchInsert(self, nums: list[int], target: int) -> int:
-            answer = 0
-            for i in range(len(nums)):
-                if nums[i] < target:
-                    answer = i + 1
-            return answer
