b596e6177812564674f3a1234ebaf98bd4c4dcf7
AdameyLaypanov <133885663+AdameyLaypanov@users.noreply.github.com>
2024-07-22 21:01:57 +0300

    update script

--- scripts/sort_easy.py
+++ scripts/sort_easy.py
@@ -23,4 +23,18 @@ for i, filename in enumerate(sorted_files, start=1):
     dst = os.path.join(path, f'{i:03d}_{new_name}')
     os.rename(src, dst)
 
+# Повторно получить список файлов после удаления префиксов
+files = os.listdir(path)
+
+# Повторно сортируем файлы по числовому префиксу
+sorted_files = sorted(files, key=get_numeric_prefix)
+
+# Переименовываем файлы в отсортированном порядке с добавлением префикса
+for i, filename in enumerate(sorted_files, start=1):
+    src = os.path.join(path, filename)
+    # Удаляем старый префикс, если он есть
+    new_name = re.sub(r'^\d+_', '', filename)
+    dst = os.path.join(path, f'{i:03d}_{new_name}')
+    os.rename(src, dst)
+
 print("Файлы успешно отсортированы и переименованы.")
