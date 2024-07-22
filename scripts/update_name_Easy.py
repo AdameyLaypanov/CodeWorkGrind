29a188be76a3d0750c8bb7ec67cb90c1280107b1
AdameyLaypanov <133885663+AdameyLaypanov@users.noreply.github.com>
2024-07-22 21:06:35 +0300

    script to remove Easy codes suffix

---
+++ scripts/update_name_Easy.py
@@ -0,0 +1,18 @@
+import os
+import re
+
+# Укажите полный путь к директории с файлами
+path = r'D:\A_Local_reps\CodeWorkGrind\LeetCode\Easy'
+
+# Получаем список файлов в директории
+files = os.listdir(path)
+
+# Переименовываем файлы, удаляя числовые префиксы
+for filename in files:
+    src = os.path.join(path, filename)
+    # Удаляем числовой префикс, если он есть
+    new_name = re.sub(r'^\d+_', '', filename)
+    dst = os.path.join(path, new_name)
+    os.rename(src, dst)
+
+print("Числовые префиксы успешно удалены из имен файлов.")
